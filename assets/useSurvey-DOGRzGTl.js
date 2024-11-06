ba9e38a2e34d304c250ad7ed526963af6ecf9901
J4viMx <87330021+J4viMx@users.noreply.github.com>
2024-11-06 22:31:59 +0000

    Build: (7effa3c) Modify some routes and add validation to prevent proceeding without answers

--- assets/useSurvey-DOGRzGTl.js
+++
@@ -1,1 +0,0 @@
-import{r,S as t}from"./index-aOwAP3n6.js";const s=()=>{const e=r.useContext(t);if(!e)throw new Error("useSurvey debe ser usado dentro de un SurveyProvider");return e};export{s as u};
