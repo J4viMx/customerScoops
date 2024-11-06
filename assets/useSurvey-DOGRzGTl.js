a3cfeb80dcafd1d49e840c16c8f52f4964ee2b41
J4viMx <87330021+J4viMx@users.noreply.github.com>
2024-11-06 21:08:54 +0000

    Build: (299ce4d) Add action to deploy

---
+++ assets/useSurvey-DOGRzGTl.js
@@ -0,0 +1,1 @@
+import{r,S as t}from"./index-aOwAP3n6.js";const s=()=>{const e=r.useContext(t);if(!e)throw new Error("useSurvey debe ser usado dentro de un SurveyProvider");return e};export{s as u};
